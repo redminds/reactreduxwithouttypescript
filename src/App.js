ee854cf6059473ca31d5ce05888582263e31d823
leela <info@redmindstech.com>
2022-04-27 23:18:59 +0530

    redux example without typescript

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css"
+import { useSelector, useDispatch } from 'react-redux';
+import { incNumber, decNumber } from './actions';
 
-function App() {
+const App = () => {
+  const myState = useSelector((state)=>state.changeTheNumber);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Increment/Decrement Counter</h1>
+      <h4>using React and Redux</h4>
+      <div className='quantity'>
+        <button className='quantity__minus' title='Decrement' onClick={()=>dispatch(decNumber())}><span>-</span></button>
+        <input name='quantity' type="text" className='quantity__input' value={myState}  />
+        <button className='quantity__plus' title='Increment' onClick={()=>dispatch(incNumber(5))} ><span>+</span></button>
+
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
